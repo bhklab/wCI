ac5d0b7b54b09d21905b5f7c6d0c5fb4527c6c13
spocks <arvindmer@gmail.com>
2017-08-01 12:01:45 -0400

    fixed delta.pred

--- R/paired.concordance.index.R
+++ R/paired.concordance.index.R
@@ -1,22 +1,22 @@
 #' Takes two numerical vectors and computes the concordance index between them
 #' by comparing the order of values for two pairs of data each time
-#' 
+#'
 #' This function return the concordance index and its p-value
 #' along with the lower and upper confidence intervals of said p-value.
 #'
-#' 
-#' @examples 
+#'
+#' @examples
 #' gdsc.auc <- summarizeSensitivityProfiles(GDSC, sensitivity.measure=‘auc_published’)
 #' xx <- summarizeSensitivityProfiles(predicted.by.model, gdsc.auc$Erlotinib)
 #'
 #' @param predictions {numeric} A vector of predicted drug responces which could be either continuous or discrete
-#' @param observations {numeric} A vector of observed continuous drug responces 
-#' @param cutoff {numeric} A drug responce threshold which is used to classify cells to sensitive vs resistant to drug. 
+#' @param observations {numeric} A vector of observed continuous drug responces
+#' @param cutoff {numeric} A drug responce threshold which is used to classify cells to sensitive vs resistant to drug.
 #' @param delta {numeric} The minimunm reliable difference between two drug sensitivity values to be considered as significantly various responses.
-#' default value for delta is picked by looking into delta auc values between biological replicates across three 
+#' default value for delta is picked by looking into delta auc values between biological replicates across three
 #' large pharmacogenomic studies, CTRPv2(370 drugs over ~15-20 cells) , GDSC(1 drug over ~600 cells), GRAY (85 drugs over ~10-50)
 #' @param alpha {numeric} alpha level to compute confidence interval
-#' @param outx {boolean} set to TRUE to not count pairs of observations tied on x as a relevant pair. 
+#' @param outx {boolean} set to TRUE to not count pairs of observations tied on x as a relevant pair.
 #' This results in a Goodman-Kruskal gamma type rank correlation.
 #' @param alternative {character} what is the alternative hypothesis? Must be one of "two.sides", "less", and "greater".
 #' @return [list] ! list of concordance index and its pvalue
@@ -35,10 +35,8 @@ paired.concordance.index <- function(predictions, observations, delta.pred=0.2, 
   for (i in seq(from = 1, to = N - 1)) {
     for (j in seq(from = i + 1, to = N)) {
       pair <- c(i, j)
-      #if ((any(predictions[pair] >= cutoff) && abs(predictions[i] - predictions[j]) >= delta) ||
-         #(any(observations[pair] >= cutoff) && abs(observations[i] - observations[j]) >= delta)) {
-        #if ((predictions[i] == predictions[j] || observations[i] == observations[j])||
-        if(abs(observations[i] - observations[j]) >= delta.obs || abs(predictions[i] - predictions[j]) >= delta[2]) { #add flag to replace 'or' behaviour with 'xor' behaviour
+        if(abs(predictions[i] - predictions[j]) >= delta.pred || abs(observations[i] - observations[j]) >= delta.obs)
+        { #add flag to replace 'or' behaviour with 'xor' behaviour
           pp <- (predictions[i] < predictions[j])
           oo <- (observations[i] < observations[j])
           if (pp == oo) {
@@ -46,7 +44,7 @@ paired.concordance.index <- function(predictions, observations, delta.pred=0.2, 
           } else {
             d[pair] <- d[pair] + 1
           }
-          
+
         } else {
           if(outx){
             u[pair] <- u[pair] + 1
@@ -54,19 +52,18 @@ paired.concordance.index <- function(predictions, observations, delta.pred=0.2, 
             d[pair] <- d[pair] + 1
           }
         }
-      #}
     }
   }
 
   C <- sum(c)
   D <- sum(d)
-  
+
   if (N < 3 || (C == 0 && D == 0)) {
     return(list("cindex"=NA, "p.value"=NA, "lower"=NA, "upper"=NA, "relevant.pairs.no"=0))
   }
-  
+
   cindex <- C / (C + D)
-  
+
   CC <- sum(c * (c - 1))
   DD <- sum(d * (d - 1))
   CD <- sum(c * d)
