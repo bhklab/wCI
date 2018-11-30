4d38f693cc69af9d07eca26838b80799a4cf2560
Zhaleh Safikhani <zhalehsafikhani@gmail.com>
2018-11-30 16:48:48 -0500

    Kernels
    The mean and standard deviation of the kernels is measured by fitting the kernels to the distribution of all the biological replicates across all pharmacogenomics datasets

---
+++ R/kernels.R
@@ -0,0 +1,7 @@
+kernel_gaussian <- function(x, m=0.0002037366, s=0.0919937995){
+  #dnorm(x, m, s)
+  (1/sqrt(2*pi*s^2))*exp(-(x-m)^2/(2*s^2))
+}
+kernel_laplace <- function(x, m=-0.001630207, b=0.060597464){
+  (1/(2*b)) * exp(-abs(x - m) / b)
+}
