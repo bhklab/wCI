5aeb9e89ab0f46fa429753f64d7a91876cef571d
Wail Ba alawi <ba3lwi@gmail.com>
2018-12-01 01:52:52 -0500

    Added a function

--- R/kernels.R
+++ R/kernels.R
@@ -5,3 +5,13 @@ kernel_gaussian <- function(x, m=0.0002037366, s=0.0919937995){
 kernel_laplace <- function(x, m=-0.001630207, b=0.060597464){
   (1/(2*b)) * exp(-abs(x - m) / b)
 }
+
+
+find.original.name <- function(fun) {
+  objects <- ls(envir = environment(fun))
+  for (i in objects) {
+    if (identical(fun, get(i, envir = environment(fun)))) {
+      return(i)
+    }
+  }
+}
