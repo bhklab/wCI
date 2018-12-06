b6ec4b4309a07ddb23d21d0477e2dcd209d2e396
Zhaleh Safikhani <zhalehsafikhani@gmail.com>
2018-12-06 14:53:11 -0500

    kernels updated

--- R/kernels.R
+++ R/kernels.R
@@ -6,7 +6,12 @@ kernel_laplace <- function(x, m=-0.001630207, b=0.060597464){
   (1/(2*b)) * exp(-abs(x - m) / b)
 }
 
-
+# Parameters obtained from CTRPv2 data; write into function parameters
+# Assume you already know the kernel because you're a badass
+# Kernel learned from CTRPv2 data and assumed to generalize. 
+kernel_sigmoid <- function(x) {
+  1/(1 + exp(-10.99319 * (x - 0.09631)))
+}
 find.original.name <- function(fun) {
   objects <- ls(envir = environment(fun))
   for (i in objects) {
