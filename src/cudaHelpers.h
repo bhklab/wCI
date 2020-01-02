db5b5d09ba30b02a5bbbfa4d3e17c831a3e0df6b
Petr Smirnov <psmirnov2000@gmail.com>
2020-01-02 12:16:10 -0500

    header file for cuda shared functions

---
+++ src/cudaHelpers.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda.h>
+#include <curand.h>
+
+
+
+static const char *curandGetErrorString(curandStatus_t error);
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true);
+
+#define gpuErrchkRand(ans) { gpuAssertRand((ans), __FILE__, __LINE__); }
+inline void gpuAssertRand(curandStatus_t code, const char *file, int line, bool abort=true);
--- src/naivePermGPU.cu
+++ src/naivePermGPU.cu
@@ -1,5 +1,5 @@
 /*
-// Fast bootstap for rCI using a naive matrix based approach.
+// Fast permutations for rCI using a naive matrix based approach.
 */
 
 
@@ -10,7 +10,84 @@
 #include <curand.h>
 #include <R.h>
 #include <Rinternals.h>
-#include "cudaHelpers.h"
+// #include "xoroshiro128+.h"
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+static const char *curandGetErrorString(curandStatus_t error)
+{
+	switch (error)
+	{
+		case CURAND_STATUS_SUCCESS:
+			return "CURAND_STATUS_SUCCESS";
+
+		case CURAND_STATUS_VERSION_MISMATCH:
+			return "CURAND_STATUS_VERSION_MISMATCH";
+
+		case CURAND_STATUS_NOT_INITIALIZED:
+			return "CURAND_STATUS_NOT_INITIALIZED";
+
+		case CURAND_STATUS_ALLOCATION_FAILED:
+			return "CURAND_STATUS_ALLOCATION_FAILED";
+
+		case CURAND_STATUS_TYPE_ERROR:
+			return "CURAND_STATUS_TYPE_ERROR";
+
+		case CURAND_STATUS_OUT_OF_RANGE:
+			return "CURAND_STATUS_OUT_OF_RANGE";
+
+		case CURAND_STATUS_LENGTH_NOT_MULTIPLE:
+			return "CURAND_STATUS_LENGTH_NOT_MULTIPLE";
+
+		case CURAND_STATUS_DOUBLE_PRECISION_REQUIRED:
+			return "CURAND_STATUS_DOUBLE_PRECISION_REQUIRED";
+
+		case CURAND_STATUS_LAUNCH_FAILURE:
+			return "CURAND_STATUS_LAUNCH_FAILURE";
+
+		case CURAND_STATUS_PREEXISTING_FAILURE:
+			return "CURAND_STATUS_PREEXISTING_FAILURE";
+
+		case CURAND_STATUS_INITIALIZATION_FAILED:
+			return "CURAND_STATUS_INITIALIZATION_FAILED";
+
+		case CURAND_STATUS_ARCH_MISMATCH:
+			return "CURAND_STATUS_ARCH_MISMATCH";
+
+		case CURAND_STATUS_INTERNAL_ERROR:
+			return "CURAND_STATUS_INTERNAL_ERROR";
+	}
+
+	return "<unknown>";
+}
+
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+	   if (code != cudaSuccess) 
+		      {
+			            fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+				          if (abort) exit(code);
+					     }
+}
+
+#define gpuErrchkRand(ans) { gpuAssertRand((ans), __FILE__, __LINE__); }
+inline void gpuAssertRand(curandStatus_t code, const char *file, int line, bool abort=true)
+{
+	if (code != CURAND_STATUS_SUCCESS)
+		{
+		        fprintf(stderr,"GPUassert: %s %s %d\n", curandGetErrorString(code), file, line);
+			if (abort) exit(code);
+	       	}
+}
+
+// #define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
+//     printf("Error at %s:%d\n",__FILE__,__LINE__);\
+//     return EXIT_FAILURE;}} while(0)
 
 
 const int numThreads = 64;
