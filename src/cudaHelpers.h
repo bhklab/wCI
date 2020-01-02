b02842ea6553bcea36012fed8888e8e28d0f54a7
Petr Smirnov <psmirnov2000@gmail.com>
2020-01-02 12:20:01 -0500

    function for inlining

--- src/cudaHelpers.h
+++ src/cudaHelpers.h
@@ -14,7 +14,19 @@ static const char *curandGetErrorString(curandStatus_t error);
      _a < _b ? _a : _b; })
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true);
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true){
+	   if (code != cudaSuccess) 
+		      {
+			            fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+				          if (abort) exit(code);
+					     }
+}
 
 #define gpuErrchkRand(ans) { gpuAssertRand((ans), __FILE__, __LINE__); }
-inline void gpuAssertRand(curandStatus_t code, const char *file, int line, bool abort=true);
+inline void gpuAssertRand(curandStatus_t code, const char *file, int line, bool abort=true){
+	if (code != CURAND_STATUS_SUCCESS)
+		{
+		        fprintf(stderr,"GPUassert: %s %s %d\n", curandGetErrorString(code), file, line);
+			if (abort) exit(code);
+	       	}
+}
